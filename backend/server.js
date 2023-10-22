0d5ba451412ce0f99e006937432a7c04f94cb057
Avishka Umesh <umesh.avishka@gmail.com>
2023-10-22 16:51:11 +0530

    added custom errors

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import connectDB from './config/db.js';
+import { errorHandler, notFound } from './middleware/errorMiddleware.js';
 import productRoutes from './routes/products.routes.js';
 
 dotenv.config();
@@ -14,6 +15,9 @@ app.get('/', (req, res) => {
 	res.send('Server is ready');
 });
 app.use('/api/products', productRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
 
 app.listen(port, () => {
 	console.log(`Serve at http://localhost:${port}`);
