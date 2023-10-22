599ff967dd99fa38d100c1fc7de8fe46c64be3b0
Avishka Umesh <umesh.avishka@gmail.com>
2023-10-22 16:35:51 +0530

    changed backend to fetch data from mongo db

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,7 @@
 import dotenv from 'dotenv';
 import express from 'express';
 import connectDB from './config/db.js';
-import products from './data/products.js';
+import productRoutes from './routes/products.routes.js';
 
 dotenv.config();
 
@@ -13,19 +13,7 @@ const app = express();
 app.get('/', (req, res) => {
 	res.send('Server is ready');
 });
-
-app.get('/api/products', (req, res) => {
-	res.send(products);
-});
-
-app.get('/api/products/:id', (req, res) => {
-	const product = products.find((x) => x._id === req.params.id);
-	if (product) {
-		res.send(product);
-	} else {
-		res.status(404).send({ message: 'Product Not Found' });
-	}
-});
+app.use('/api/products', productRoutes);
 
 app.listen(port, () => {
 	console.log(`Serve at http://localhost:${port}`);
