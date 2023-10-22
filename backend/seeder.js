ea9f5d7fb0b6e6ef5a24e3ea90e45337aaf8195a
Avishka Umesh <umesh.avishka@gmail.com>
2023-10-22 16:15:18 +0530

    added data base seeder

---
+++ backend/seeder.js
@@ -0,0 +1,63 @@
+import colors from 'colors';
+import dotenv from 'dotenv';
+
+import products from './data/products.js';
+import users from './data/users.js';
+
+import Order from './models/orderModel.js';
+import Product from './models/productModel.js';
+import User from './models/userModel.js';
+
+import connectDB from './config/db.js';
+
+dotenv.config();
+
+connectDB();
+
+const importData = async () => {
+	try {
+		// Clear the database
+		await Order.deleteMany();
+		await Product.deleteMany();
+		await User.deleteMany();
+
+		// Insert users
+		const createdUsers = await User.insertMany(users);
+
+		// Get admin user
+		const adminUser = createdUsers[0]._id;
+
+		// Insert products
+		const sampleProducts = products.map((product) => {
+			return { ...product, user: adminUser };
+		});
+		await Product.insertMany(sampleProducts);
+
+		console.log('Data Imported!'.green.inverse);
+		process.exit();
+	} catch (error) {
+		console.error(`${error}`.red.inverse);
+		process.exit(1);
+	}
+};
+
+const destroyData = async () => {
+	try {
+		// Clear the database
+		await Order.deleteMany();
+		await Product.deleteMany();
+		await User.deleteMany();
+
+		console.log('Data Destroyed!'.red.inverse);
+		process.exit();
+	} catch (error) {
+		console.error(`${error}`.red.inverse);
+		process.exit(1);
+	}
+};
+
+if (process.argv[2] === '-d') {
+	destroyData();
+} else {
+	importData();
+}
