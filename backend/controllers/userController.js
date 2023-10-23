9f8303444a08223f071d8774cc4523d9901dad50
Avishka Umesh <umesh.avishka@gmail.com>
2023-10-23 15:23:43 +0530

    implemented auth user API method

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,7 +1,47 @@
+import jwt from 'jsonwebtoken';
 import asyncHandler from '../middleware/asyncHandler.js';
+import User from '../models/userModel.js';
 
+/**
+ * @function
+ * @async
+ * @desc Auth user & get token
+ * @param {Response} res - The Express.js response object.
+ * @param {Request} req - The Express.js request object.
+ * @route POST /api/users/auth
+ * @access Public
+ * @returns {Object} - user object
+ */
 const authUser = asyncHandler(async (req, res) => {
-    res.send('auth user');
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email });
+
+    if (user && (await user.matchPassword(password))) {
+        const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+            expiresIn: '1d',
+        });
+
+        // set JWT as HTTP-Only cookie
+        res.cookie('jwt', token, {
+            httpOnly: true,
+            secure:
+                process.env.NODE_ENV !== 'development' ||
+                process.env.NODE_ENV !== 'test',
+            sameSite: 'strict',
+            maxAge: 24 * 60 * 60 * 1000, // 1 day
+        });
+
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+            isAdmin: user.isAdmin,
+        });
+    } else {
+        res.status(401);
+        throw new Error('Invalid email or password');
+    }
 });
 
 const registerUser = asyncHandler(async (req, res) => {
