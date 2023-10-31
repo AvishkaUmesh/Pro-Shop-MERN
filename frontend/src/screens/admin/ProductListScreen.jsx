c702df88fce250e7f9a5c3bd6e0882824153bfc6
Avishka Umesh <umesh.avishka@gmail.com>
2023-10-31 14:11:30 +0530

    product list loading bug fixed

--- frontend/src/screens/admin/ProductListScreen.jsx
+++ frontend/src/screens/admin/ProductListScreen.jsx
@@ -60,58 +60,63 @@ const ProductListScreen = () => {
             {createError && <Message variant="danger">{createError}</Message>}
             {deleteError && <Message variant="danger">{deleteError}</Message>}
             {error && <Message variant="danger">{error}</Message>}
+            {!isLoading && products.length === 0 && (
+                <Message variant="info">No products found</Message>
+            )}
 
-            <Table striped bordered hover responsive className="table-sm">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>NAME</th>
-                        <th>PRICE</th>
-                        <th>CATEGORY</th>
-                        <th>BRAND</th>
+            {!isLoading && products.length > 0 && (
+                <Table striped bordered hover responsive className="table-sm">
+                    <thead>
+                        <tr>
+                            <th>ID</th>
+                            <th>NAME</th>
+                            <th>PRICE</th>
+                            <th>CATEGORY</th>
+                            <th>BRAND</th>
 
-                        <th></th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {products &&
-                        products.map((product) => (
-                            <tr key={product._id}>
-                                <td>{product._id}</td>
-                                <td>{product.name}</td>
-                                <td>${product.price}</td>
-                                <td>{product.category}</td>
-                                <td>{product.brand}</td>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {products &&
+                            products.map((product) => (
+                                <tr key={product._id}>
+                                    <td>{product._id}</td>
+                                    <td>{product.name}</td>
+                                    <td>${product.price}</td>
+                                    <td>{product.category}</td>
+                                    <td>{product.brand}</td>
 
-                                <td>
-                                    <LinkContainer
-                                        to={`/admin/product/${product._id}/edit`}
-                                    >
-                                        <Button
-                                            variant="light"
-                                            className="btn-sm mx-2"
+                                    <td>
+                                        <LinkContainer
+                                            to={`/admin/product/${product._id}/edit`}
                                         >
-                                            <FaEdit />
+                                            <Button
+                                                variant="light"
+                                                className="btn-sm mx-2"
+                                            >
+                                                <FaEdit />
+                                            </Button>
+                                        </LinkContainer>
+                                        <Button
+                                            variant="danger"
+                                            className="btn-sm"
+                                            onClick={() =>
+                                                deleteHandler(product._id)
+                                            }
+                                        >
+                                            <FaTrash
+                                                style={{
+                                                    color: 'white',
+                                                }}
+                                            />
                                         </Button>
-                                    </LinkContainer>
-                                    <Button
-                                        variant="danger"
-                                        className="btn-sm"
-                                        onClick={() =>
-                                            deleteHandler(product._id)
-                                        }
-                                    >
-                                        <FaTrash
-                                            style={{
-                                                color: 'white',
-                                            }}
-                                        />
-                                    </Button>
-                                </td>
-                            </tr>
-                        ))}
-                </tbody>
-            </Table>
+                                    </td>
+                                </tr>
+                            ))}
+                    </tbody>
+                </Table>
+            )}
         </>
     );
 };
